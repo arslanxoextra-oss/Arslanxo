7434f7ef5d2e75ce664d3f2a0e29f60732295648
arslanxoextra-oss <arslanxoextra@gmail.com>
2026-09-20 19:30:06 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     minSdk = 24
     targetSdk = 36
     versionCode = System.getenv("VERSION_CODE")?.toIntOrNull() ?: 2
-    versionName = System.getenv("VERSION_NAME") ?: "1.1"
+    versionName = System.getenv("VERSION_NAME") ?: "1.0.6"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
