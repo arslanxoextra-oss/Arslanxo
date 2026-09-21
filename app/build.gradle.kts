fc7d0c17da2e1e63449ea3f47f27482e37fc1320
arslanxoextra-oss <arslanxoextra@gmail.com>
2026-09-21 13:49:40 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 24
         targetSdk = 36
 
-        versionCode = System.getenv("VERSION_CODE")?.toIntOrNull() ?: 2
+        versionCode = System.getenv("VERSION_CODE")?.toIntOrNull() ?: 1
         versionName = System.getenv("VERSION_NAME") ?: "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
